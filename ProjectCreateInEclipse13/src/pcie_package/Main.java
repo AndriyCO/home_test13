424c5f5abf01dc7d87654d33beda794842b60dcd
AndriyCO <Aolijnyk73@gmail.com>
2017-07-22 02:12:57 +0300

    second comit in main line

--- ProjectCreateInEclipse13/src/pcie_package/Main.java
+++ ProjectCreateInEclipse13/src/pcie_package/Main.java
@@ -1,5 +1,7 @@
 package pcie_package;
 
 public class Main {
-
+	public static void main(String[] args) {
+		System.out.println("Changes in the project for the master-branch of the project");
+	}
 }
